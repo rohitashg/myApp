316c1655041034ec363cff0f3469878815ded603
Anthony Petti <apetti@tk.co>
2017-06-07 16:13:20 +0530

    save data in db

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Http, Response, Headers, RequestOptions} from "@angular/http";
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -11,12 +12,21 @@ export class ContactComponent {
 constructor(private http: Http) {
        
 }
-   /*submitForm(){
-   	let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-        return this.http.post('http://localhost/angular2/angularApi/signup.php',{book} , options)
-                   .map(this.extractData)
-                   .catch(this.handleErrorObservable);
-   console.log("come here") 
-  }*/
+    contact : Object= {};
+   submitForm(){
+  
+    if(this.contact){
+      console.log(this.contact);
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+
+      return this.http.post('http://localhost/angular2/angularApi/signup.php',this.contact,options)
+      .toPromise()
+      .then(response => response.json())
+    }
+    
+                   //.map()
+                   //.catch(this.handleErrorObservable);
+  
+  }
 }
